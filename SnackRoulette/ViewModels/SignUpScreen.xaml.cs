7f625510072d186f31c41339d86d13479c2d2f99
SiuCheungChung <n9574042@qut.edu.au>
2019-09-12 11:23:30 +1000

    Add some entries and labels on Sign up screen

--- SnackRoulette/ViewModels/SignUpScreen.xaml.cs
+++ SnackRoulette/ViewModels/SignUpScreen.xaml.cs
@@ -10,8 +10,38 @@ namespace SnackRoulette.Views
         public SignUpScreen()
         {
             InitializeComponent();
-
-   
         }
+
+        public string FullName
+        {
+            get { return FullName; }
+            set { FullName = value; }
+        }
+
+        public string Email
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+        public string Password
+        {
+            get { return Password; }
+            set { Password = value; }
+        }
+
+        public string Confirm
+        {
+            get { return Confirm; }
+            set { Confirm = value; }
+        }
+
+        void Handle_Submit(object sender, System.EventArgs e)
+        {
+            //Navigation.PushAsync(new OrderScreen());
+        }
+
+
+
     }
 }
