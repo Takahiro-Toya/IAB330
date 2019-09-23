4993da4632a271036da77f9e8fb02384bb1cee02
Callum Woodland <n10067906@qut.edu.au>
2019-09-23 16:57:06 +1000

    reworked layout

--- SnackRoulette/ViewModels/MapPage.xaml.cs
+++ SnackRoulette/ViewModels/MapPage.xaml.cs
@@ -5,90 +5,20 @@ using System.Diagnostics;
 using Plugin.Permissions.Abstractions;
 using Plugin.Geolocator;
 using MapPage;
+using System.Text;
 
 namespace SnackRoulette
 {
     public class MapPage : ContentPage
     {
-        CustomMap customMap;
-        public MapPage()
+        public void FindRestaurants(View v)
         {
-            customMap = new CustomMap
-            {
-                
-                IsShowingUser = true, //Dont Activate this until GPS permissions are met (It will just crash)
-                HeightRequest = 100,
-                WidthRequest = 960,
-                VerticalOptions = LayoutOptions.FillAndExpand
-            };
+            StringBuilder stringSearch = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch?/xml?");
+            stringSearch.Append("&radius =" + 1000);
+            stringSearch.Append("&keyword=" + "restaurant");
+            stringSearch.Append("&key="+ "AIzaSyB-ZzI-vRGNRgIkay97Cc42v5P_gxD_t5w");
 
-
-            //Starting region of the map, figure out how to set that to GPS
-            var position = new Position(-27.477730, 153.029065);
-
-            // add the slider for adjusting search radius
-            var slider = new Slider(1, 18, 1);
-            slider.ValueChanged += (sender, e) => {
-
-            };
-
-            var pin = new Pin
-            {
-                Type = PinType.Place,
-                Position = position,
-                Label = "QUT Gardens Point Campus",
-                Address = "2 George St, Brisbane City QLD 4000"
-            };
-
-            
-            customMap.Circle = new CustomCircle
-            {
-                Position = position,
-                Radius = 1000
-            };
-
-            customMap.Pins.Add(pin);
-            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMiles(1.0)));
-
-
-            // create map style buttons
-            var street = new Button { Text = "Street" };
-            var hybrid = new Button { Text = "Hybrid" };
-            street.Clicked += HandleClicked;
-            hybrid.Clicked += HandleClicked;
-            var segments = new StackLayout
-            {
-                
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                Orientation = StackOrientation.Horizontal,
-                Spacing = 50,
-                Children = { street, hybrid }
-            };
-
-
-            // constructing all above functions (Need to convert this into XML, I was lazy)
-            var stack = new StackLayout { Spacing = 0 };
-            stack.Children.Add(customMap);
-            stack.Children.Add(slider);
-            stack.Children.Add(segments);
-            Content = stack;
-
-
- 
-        }
-
-        void HandleClicked(object sender, EventArgs e)
-        {
-            var b = sender as Button;
-            switch (b.Text)
-            {
-                case "Street":
-                    customMap.MapType = MapType.Street;
-                    break;
-                case "Hybrid":
-                    customMap.MapType = MapType.Hybrid;
-                    break;
-            }
+            String search = stringSearch.ToString();
         }
     }
 }
