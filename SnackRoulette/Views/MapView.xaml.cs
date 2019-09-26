6a82c3c63d255921674be6d84f19250850213d0a
scott1909 <scottcameron.bayvel@connect.qut.edu.au>
2019-09-27 09:04:16 +1000

    * MapView.xaml: updated MapView from scott
    * MapView.xaml.cs:

---
+++ SnackRoulette/Views/MapView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SnackRoulette.Views
+{
+    public partial class MapView : ContentPage
+    {
+        public MapView()
+        {
+            InitializeComponent();
+        }
+    }
+}
